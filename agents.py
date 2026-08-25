dbe086597b27355abd2b7a751370ffd785e36f50
riya-py <lowkeywhocares@gmail.com>
2026-08-25 17:21:29 +0530

    updated agents

--- agents.py
+++ agents.py
@@ -8,8 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #model setup 
-llm = ChatOpenAI(model = "gpt-4o-mini",temperature=0)
-
+llm = ChatOpenAI(model = "openai/gpt-oss-120b",temperature=0)
 
 #1st agent 
 def build_search_agent():
